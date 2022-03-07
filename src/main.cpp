49f35e11047cabe1f5589293a2b5268c80f2f81f
Matt Hackmann <matt.hackmann@gmail.com>
2022-03-06 21:21:32 -0800

    basic peloton light mode server

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <FastLED.h>
-// #include <WiFi.h>
-// #include <WiFiClient.h>
-// #include <WebServer.h>
-// #include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <uri/UriBraces.h>
 
-#define NUM_LEDS   120
+#include <credentials.h>
+
+#define NUM_LEDS   300
 #define DATA_PIN   22
 
 CRGB leds[NUM_LEDS];
@@ -14,19 +17,54 @@ CRGB leds[NUM_LEDS];
 #define RESISTANCE_MAX   70.0
 #define RESISTANCE_MIN   25.0
 #define MAX_SPEED        12
+// ~60fps
+#define FPS_DELAY        16
 const int16_t RESISTANCE_RANGE = RESISTANCE_MAX - RESISTANCE_MIN;
 const uint8_t BRIGHTNESS[8] = { 32, 64, 128, 192, 255, 192, 128, 64 };
 
-uint32_t timer;
+uint32_t globalTimer;
 uint16_t hue;
 CRGB color;
+WebServer server(80);
+uint16_t cadence = 0;
+uint16_t resistance = 0;
+
+void setupNetwork() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(HUE_MIN, 255, BRIGHTNESS[(globalTimer / 2) & 0x7]);
+    }
+    FastLED.show();
+    delay(FPS_DELAY);
+    globalTimer++;
+  }
+
+  Serial.print("Connected to WiFi: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDSN responder starter");
+  }
+
+  server.on(UriBraces("/bicycle/{}/{}"), []() {
+    Serial.println("Adjusting Peloton lighting");
+    cadence = server.pathArg(0).toInt();
+    resistance = server.pathArg(1).toInt();
+    server.send(200, "text/html", "OK");
+  });
+
+  server.begin();
+}
 
 void setup() {
   // put your setup code here, to run once:
   FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);
   Serial.begin(115200);
 
-  timer = RESISTANCE_MIN;
+  setupNetwork();
+
+  globalTimer = 0;
 }
 
 uint8_t getHueForResistance(uint8_t resistance) {
@@ -43,14 +81,12 @@ uint8_t getHueForResistance(uint8_t resistance) {
     retVal = hue;
   }
 
-  Serial.println(retVal);
-
   return retVal;
 }
 
 void setLightsForSpeedAndResistance(uint8_t speed, uint8_t resistance) {
   uint8_t speedModifier = abs((speed / 10) - MAX_SPEED) + 1;
-  uint8_t timerSpeedReduced = timer / speedModifier;
+  uint8_t timerSpeedReduced = globalTimer / speedModifier;
   uint8_t resistanceHue = getHueForResistance(resistance);
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CHSV(resistanceHue, 255, BRIGHTNESS[(i + timerSpeedReduced) & 0x7]);
@@ -59,7 +95,12 @@ void setLightsForSpeedAndResistance(uint8_t speed, uint8_t resistance) {
 }
 
 void loop() {
-  timer++;
-  setLightsForSpeedAndResistance(120, 35);
-  delay(16);
+  globalTimer++;
+  server.handleClient();
+
+  if (cadence > 0 && resistance > 0) {
+    setLightsForSpeedAndResistance(cadence, resistance);
+  }
+
+  delay(FPS_DELAY);
 }
