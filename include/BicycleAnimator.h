5535771c54590ea1a73c45a0c5d3626f97649a5d
Matt Hackmann <matt.hackmann@gmail.com>
2022-03-20 19:06:03 -0700

    transition to new values

--- include/BicycleAnimator.h
+++ include/BicycleAnimator.h
@@ -8,20 +8,20 @@
 #define RESISTANCE_MAX   70.0
 #define RESISTANCE_MIN   25.0
 #define MAX_SPEED        12
-#define TARGET_DURATION  60 // 60 frames to catch up on a target stats change
+#define TARGET_DURATION  60.0 // 60 frames to catch up on a target stats change
 
 class BicycleAnimator: public Animator {
   private:
-    uint16_t targetAnimationTimer;
-    uint16_t targetCadence;
-    uint16_t targetResistance;
-    uint16_t currentCadence;
-    uint16_t currentResistance;
+    uint32_t targetAnimationTimer;
+    uint8_t targetCadence;
+    uint8_t targetResistance;
+    uint8_t currentCadence;
+    uint8_t currentResistance;
 
   public:
     BicycleAnimator(uint16_t numLeds, CRGB *leds);
     void renderFrame(uint32_t timer);
-    void setTargetStatus(uint16_t cadence, uint16_t resistance, uint32_t timer);
+    void setTargetStatus(uint8_t cadence, uint8_t resistance, uint32_t timer);
 };
 
 #endif
--- src/BicycleAnimator.cpp
+++ src/BicycleAnimator.cpp
@@ -31,19 +31,31 @@ BicycleAnimator::BicycleAnimator(uint16_t numLeds, CRGB *leds) {
 }
 
 void BicycleAnimator::renderFrame(uint32_t timer) {
-  uint8_t speedModifier = abs((this->currentCadence / 10) - MAX_SPEED) + 1;
+  uint8_t cadence = this->currentCadence;
+  uint8_t resistance = this->currentResistance;
+
+  if (this->targetAnimationTimer > timer) {
+    double startTime = this->targetAnimationTimer - TARGET_DURATION;
+    double timerDelta = (timer - startTime) / TARGET_DURATION;
+    int8_t cadenceDelta = this->targetCadence - this->currentCadence;
+    int8_t resistanceDelta = this->targetResistance - this->currentResistance;
+    cadence = this->currentCadence + (int8_t) (timerDelta * cadenceDelta);
+    resistance = this->currentResistance + (int8_t) (timerDelta * resistanceDelta);
+  } else {
+    this->currentCadence = cadence = this->targetCadence;
+    this->currentResistance = resistance = this->targetResistance;
+  }
+
+  uint8_t speedModifier = abs((cadence / 10) - MAX_SPEED) + 1;
   uint8_t timerSpeedReduced = timer / speedModifier;
-  uint8_t resistanceHue = getHueForResistance(this->currentResistance);
+  uint8_t resistanceHue = getHueForResistance(resistance);
   for (int i = 0; i < this->numLeds; i++) {
     this->leds[i] = CHSV(resistanceHue, 255, BRIGHTNESS[(i + timerSpeedReduced) & 0x7]);
   }
 }
 
-void BicycleAnimator::setTargetStatus(uint16_t cadence, uint16_t resistance, uint32_t timer) {
+void BicycleAnimator::setTargetStatus(uint8_t cadence, uint8_t resistance, uint32_t timer) {
   this->targetCadence = cadence;
   this->targetResistance = resistance;
   this->targetAnimationTimer = timer + TARGET_DURATION;
-  // debug
-  this->currentCadence = cadence;
-  this->currentResistance = resistance;
 }
